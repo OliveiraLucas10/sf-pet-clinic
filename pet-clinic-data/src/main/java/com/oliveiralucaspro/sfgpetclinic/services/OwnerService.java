39f8620e89bc7f9ecfd68a2c920525d66c0f78f3
Lucas Oliveira <lucas.oliveira@celfocus.com>
2020-03-29 10:16:27 +0100

    Refactoring to common crud interface. closes#1

--- pet-clinic-data/src/main/java/com/oliveiralucaspro/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/oliveiralucaspro/sfgpetclinic/services/OwnerService.java
@@ -1,17 +1,9 @@
 package com.oliveiralucaspro.sfgpetclinic.services;
 
-import java.util.Set;
-
 import com.oliveiralucaspro.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner saveOwner(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/oliveiralucaspro/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/oliveiralucaspro/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.oliveiralucaspro.sfgpetclinic.services;
 
-import java.util.Set;
-
 import com.oliveiralucaspro.sfgpetclinic.model.Vet;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet saveOwner(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
