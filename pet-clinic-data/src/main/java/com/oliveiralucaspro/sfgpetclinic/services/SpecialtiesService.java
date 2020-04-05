66b9c98a39b024aefb0e04dbc9d8c9cb7a69d47c
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-05 20:53:54 +0100

    Added Specialties Map Impl service. closes #26

---
+++ pet-clinic-data/src/main/java/com/oliveiralucaspro/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.oliveiralucaspro.sfgpetclinic.services;
+
+import com.oliveiralucaspro.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
